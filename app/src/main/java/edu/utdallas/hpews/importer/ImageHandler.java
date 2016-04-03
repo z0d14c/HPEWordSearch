f5788f2d4da6d717001a08ae31fb13aad23ab871
Frances Marie Imperial <imperialsoup@gmail.com>
2016-04-03 12:32:10 -0600

    Restructured code to align with design spec

--- app/src/main/java/edu/utdallas/hpews/importer/ImageHandler.java
+++ app/src/main/java/edu/utdallas/hpews/importer/ImageHandler.java
@@ -1,22 +1,52 @@
 package edu.utdallas.hpews.importer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.media.Image;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+
+import edu.utdallas.hpews.R;
 
 /**
  * Created by imper on 3/26/2016.
  */
-public class ImageHandler {
-    public ImageHandler(){}
+public class ImageHandler extends ContextWrapper {
 
-    public Bitmap takePicture(){
-        //TODO: implement
-        //Intent intent = new Intent();
-        throw new UnsupportedOperationException();
+    Bitmap image;
+    Activity activityRef;
+    public ImageHandler(Context context) {
+        super(context);
+        activityRef = (Activity)this.getBaseContext();
     }
 
-    public Bitmap choosePicture(){
-        //TODO: implement
-        throw new UnsupportedOperationException();
+    private static final int PICK_IMAGE_REQUEST = 1;
+    public void choosePicture() {
+        Intent pickPhotoIntent = new Intent();
+        pickPhotoIntent.setType("image/*");
+        pickPhotoIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+        activityRef.startActivityForResult(Intent.createChooser(pickPhotoIntent, "Select Picture"), PICK_IMAGE_REQUEST);
     }
+
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    public void takePicture() {
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePhotoIntent.resolveActivity(getPackageManager()) != null){
+            activityRef.startActivityForResult(takePhotoIntent, REQUEST_IMAGE_CAPTURE);
+        }
+        else{
+            Log.e("ImageHandler", "Need permission to use Camera. Check AndroidManifest.xml.");
+        }
+    }
+
 }

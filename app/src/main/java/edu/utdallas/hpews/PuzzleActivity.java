114bf0adb17800c50cfb4d79fb2cc57bde5049b4
thomas grice <augustus.grice@gmail.com>
2016-04-08 22:16:13 -0500

    * prototype puzzle display
    still need actual puzzle model etc., but this works

--- app/src/main/java/edu/utdallas/hpews/PuzzleActivity.java
+++ app/src/main/java/edu/utdallas/hpews/PuzzleActivity.java
@@ -1,21 +1,121 @@
 package edu.utdallas.hpews;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import edu.utdallas.hpews.R;
 
 public class PuzzleActivity extends AppCompatActivity {
-
+    String LogTag = "PuzzleActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puzzle);
+        final Context context = this;
+        final int dimensions = 9;
+        final LinearLayout WordPuzzleLayout = (LinearLayout) findViewById(R.id.WordPuzzleLayout);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        Integer width = size.x;
+        Integer height = size.y;
+        GridView gridview = (GridView) findViewById(R.id.WordPuzzle);
+        if (gridview != null) {
+            Log.v(LogTag, "activating grid adapter");
+            String[] letters = new String[81];
+            for(int i = 0; i<81; i++) {
+                letters[i] = "A";
+            }
+            gridview.setNumColumns(dimensions);
+            gridview.setAdapter(new PuzzleAdapter(context, dimensions, width, height, letters));
+        }
+        Log.v(LogTag, width.toString());
+        Log.v(LogTag, height.toString());
+        // need Global layout listener to determine WordPuzzleLayout when it renders
+//        ViewTreeObserver vto = WordPuzzleLayout.getViewTreeObserver();
+//        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//            @Override
+//            public void onGlobalLayout() {
+//                int width = WordPuzzleLayout.getMeasuredWidth();
+//                int height = WordPuzzleLayout.getMeasuredHeight();
+//                GridView gridview = (GridView) findViewById(R.id.WordPuzzle);
+//                if (gridview != null) {
+//                    Log.v(LogTag, "activating grid adapter");
+//                    gridview.setNumColumns(dimensions);
+//                    gridview.setAdapter(new PuzzleAdapter(context, dimensions, width, height));
+//                }
+//            }
+//        });
+//        gridview.setOnItemClickListener(new OnItemClickListener() {
+//            public void onItemClick(AdapterView<?> parent, View v,
+//                                    int position, long id) {
+//                Toast.makeText(HelloGridView.this, "" + position,
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+    }
 
+    private class PuzzleAdapter extends BaseAdapter {
+        private Context context;
+        private String[] PuzzleLetters;
+        private int dimensions;
+        private int width;
+        private int height;
+        public PuzzleAdapter(Context context, int dimensions, int width, int height, String[] letters) {
+            this.dimensions = dimensions;
+            this.context = context;
+            this.PuzzleLetters = letters;
+        }
+
+        // return number of items (characters) in puzzle
+        public int getCount() {
+            return this.PuzzleLetters.length;
+        }
+        //
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        //return dimensions
+        //TODO: make return puzzle.getDimension()
+        public int getDimension() {
+            return this.dimensions;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv;
+//            if (convertView == null) {
+            tv = new TextView(context);
+//            tv.setLayoutParams(new GridView.LayoutParams(width/dimensions, height/dimensions));
+            tv.setLayoutParams(new GridView.LayoutParams(85, 85));
+//            }
+//            else {
+//                tv = (TextView) convertView;
+//            }
+
+            tv.setText(this.PuzzleLetters[position]);
+            return tv;
+        }
     }
 
 }
+
+
